7ba3cca835549f0207b4375d19aef5ca98ca741c
sagar <sagarchoudhari909@gmail.com>
2024-04-15 21:08:10 +0530

    updation in the code according to the figma design.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
 import { OtpScreenComponent } from './components/otp-screen/otp-screen.component';
+import { LibraryComponent } from './components/library/library.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'login', pathMatch: 'full'},
     { path: 'login', component: LoginComponent},
     { path: 'sign-up', component: SignupComponent},
-    { path: 'otp', component: OtpScreenComponent}
+    { path: 'otp', component: OtpScreenComponent},
+    { path: 'library', component: LibraryComponent }
 ];
